81ddceb04b80b21e626b5cb1401d56b647f0d99e
Alexandre Freire García <xandrefreire@gmail.com>
2016-07-13 22:31:19 +0200

    Initial Commit

---
+++ KataFizzBuzzTests/KataFizzBuzzTests.swift
@@ -0,0 +1,36 @@
+//
+//  KataFizzBuzzTests.swift
+//  KataFizzBuzzTests
+//
+//  Created by Alexandre Freire García on 13/7/16.
+//  Copyright © 2016 Alexandre Freire García. All rights reserved.
+//
+
+import XCTest
+@testable import KataFizzBuzz
+
+class KataFizzBuzzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
